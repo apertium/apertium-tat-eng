a73a095eef1e9dcbf1a14b938fbfd4106efed829
Ilnar Salimzianov <ilnar@selimcan.org>
2018-04-02 19:02:52 +0300

    minor

--- explode_ambig.py
+++ explode_ambig.py
@@ -3,7 +3,7 @@
 from streamparser.streamparser import parse_file, reading_to_string
 import sys
 for blank, lu in parse_file(sys.stdin, with_text=True):
-    print(blank+" ".join("^{}/{}$".format(lu.wordform, readingToString(r))
+    print(blank+" ".join("^{}/{}$".format(lu.wordform, reading_to_string(r))
                          for r in lu.readings),
           end="")
 print("")
