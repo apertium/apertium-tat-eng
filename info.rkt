b87f26807b61de9766dd8967563516862ecb1425
Ilnar Salimzianov <ilnar@selimcan.org>
2019-04-06 19:42:01 +0300

    add racket pkg info and interface

---
+++ info.rkt
@@ -0,0 +1,7 @@
+#lang info
+(define collection "apertium-tat-eng")
+(define deps '("base" "rash"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define pkg-desc "Tatar-English machine translator")
+(define version "0.0")
+(define pkg-authors '())
---
+++ main.rkt
@@ -0,0 +1,49 @@
+#lang racket
+
+; Racket interface for apertium-tat-eng;
+
+; REQUIRES: apertiumpp package.
+; https://taruen.github.io/apertiumpp/apertiumpp/ gives info on how to install
+; it.
+
+(provide tat-eng
+         tat-eng-from-pretransfer-to-biltrans
+         tat-eng-from-t1x-to-postgen)
+
+(require pkg/lib
+         rackunit
+         rash
+         apertiumpp/streamparser)
+
+(define (symbol-append s1 s2)
+  (string->symbol (string-append (symbol->string s1) (symbol->string s2))))
+
+(define A-TAT-ENG './)
+(define A-TAT-ENG-BIL (symbol-append A-TAT-ENG 'tat-eng.autobil.bin))
+(define A-TAT-ENG-T1X (symbol-append A-TAT-ENG 'apertium-eng-kaz.tat-eng.t1x))
+(define A-TAT-ENG-T1X-BIN (symbol-append A-TAT-ENG 'tat-eng.t1x.bin))
+(define A-TAT-ENG-T2X (symbol-append A-TAT-ENG 'apertium-eng-kaz.tat-eng.t2x))
+(define A-TAT-ENG-T2X-BIN (symbol-append A-TAT-ENG 'tat-eng.t2x.bin))
+(define A-TAT-ENG-T3X (symbol-append A-TAT-ENG 'apertium-eng-kaz.tat-eng.t3x))
+(define A-TAT-ENG-T3X-BIN (symbol-append A-TAT-ENG 'tat-eng.t3x.bin))
+(define A-TAT-ENG-GEN (symbol-append A-TAT-ENG 'tat-eng.autogen.bin))
+
+(define (tat-eng s)
+  (parameterize ([current-directory (pkg-directory "apertium-tat-eng")])
+    (rash
+     "echo (values s) | apertium -d . tat-eng")))
+
+(define (tat-eng-from-pretransfer-to-biltrans s)
+  (parameterize ([current-directory (pkg-directory "apertium-tat-eng")])
+    (rash
+     "echo (values s) | apertium-pretransfer | "
+     "lt-proc -b (values A-TAT-ENG-BIL)")))
+
+(define (tat-eng-from-t1x-to-postgen s)
+  (parameterize ([current-directory (pkg-directory "apertium-tat-eng")])
+    (rash
+     "echo (values s) | "
+     "apertium-transfer -b (values A-TAT-ENG-T1X) (values A-TAT-ENG-T1X-BIN) | "
+     "apertium-interchunk (values A-TAT-ENG-T2X) (values A-TAT-ENG-T2X-BIN) | "
+     "apertium-postchunk (values A-TAT-ENG-T3X) (values A-TAT-ENG-T3X-BIN) | "
+     "lt-proc -g (values A-TAT-ENG-GEN)")))
